957bd98e73e199ab826d2695559eb3cb9509ef3d
ashleymazzonna <ashleym5243@gmail.com>
2019-10-12 20:56:50 -0400

    add image, edit header

---
+++ index.js
@@ -0,0 +1,39 @@
+/**************************
+ *** CALCULATOR NUMBERS ***
+ *************************/
+const zero = parseInt(document.getElementById('zero').innerHTML);
+const one = parseInt(document.getElementById('one').innerHTML);;
+const two = parseInt(document.getElementById('two').innerHTML);
+const three = parseInt(document.getElementById('three').innerHTML);
+const four = parseInt(document.getElementById('four').innerHTML);
+const five = parseInt(document.getElementById('five').innerHTML);
+const six = parseInt(document.getElementById('six').innerHTML);
+const seven = parseInt(document.getElementById('seven').innerHTML);
+const eight = parseInt(document.getElementById('eight').innerHTML);
+const nine = parseInt(document.getElementById('nine').innerHTML);
+/******************************
+ *** CALCULATOR OPERATORS ***
+ ****************************/
+const times = document.getElementById('multiply');
+const divide = document.getElementById('divide');
+const add = document.getElementById('add');
+const subtract = document.getElementById('subtract');
+/**************ANSWER *********/
+const answer = document.getElementById('answer');
+
+
+function addFunc(x, y) {
+
+
+
+    return x + y;
+}
+
+
+
+
+
+
+
+
+
