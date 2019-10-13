7759ba005282e3d5f9948d9d23fc03080ac4fbaf
Ashley <31573757+ashleymazzonna@users.noreply.github.com>
2019-10-13 19:00:22 -0400

    add functionality to clear and backspace btn

--- index.js
+++ index.js
@@ -24,22 +24,55 @@ const clearBtn = document.getElementById('clear-btn');
 const equalBtn = document.getElementById('equals');
 
 /**************ANSWER *********/
-const answerEl = document.getElementById('answer');
+let answerEl = document.getElementById('answer');
 
 const numberBtns = document.getElementsByClassName('number-btn');
 const operatorBtns = document.getElementsByClassName('operator-btn');
 
-let currentDisplay = [];
-let pendingNum;
+let displayVal = '0';
+let pendingVal;
 let evalStringArray = [];
 
+const updateDisplayVal = (clickedObj) => {
+    let btnText = clickedObj.target.innerText;
+
+    if (displayVal === '0')
+        displayVal = '';
+
+    displayVal += btnText;
+    answerEl.innerText = displayVal;
+
+}
+
 for (var i = 0; i < numberBtns.length; i++) {
-    numberBtns[i].addEventListener('click', updateDisplay, false);
+    numberBtns[i].addEventListener('click', updateDisplayVal, false);
 };
 
 // for (let i = 0; i < operatorBtns.length; i++) {
 //     operatorBtns[i].addEventListener('click', , false);
 // };
+
+
+clearBtn.onclick = () => {
+    displayVal = '0';
+    pendingVal = undefined;
+    evalStringArray = [];
+    answerEl.innerHTML = '0';
+}
+
+backBtn.onclick = () => {
+    let lengthOfDisplayVal = displayVal.length;
+    displayVal = displayVal.slice(0, lengthOfDisplayVal - 1);
+    answerEl.innerText = displayVal;
+
+    if (displayVal === '')
+        displayVal = '0';
+
+    answerEl.innerText = displayVal;
+
+
+}
+
 
 
 
